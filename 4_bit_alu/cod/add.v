78dd7801a162aca5b9f928a0106065f80cc2e78c
Shivaprasad <shivaprasadvibhooti03@gmail.com>
2025-12-24 04:02:29 +0530

    Delete 4_bit_alu/cod directory

--- 4_bit_alu/cod/add.v
+++
@@ -1,6 +0,0 @@
-module add(input [3:0] A, B,
-           output [7:0] result);
-
-    assign result = A + B;
-
-endmodule
--- 4_bit_alu/cod/mul.v
+++
@@ -1,6 +0,0 @@
-module mul(input [3:0] A, B,
-           output [7:0] result);
-
-    assign result = A * B;
-
-endmodule
